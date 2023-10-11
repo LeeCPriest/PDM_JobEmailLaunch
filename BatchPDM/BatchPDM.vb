47d4e46cf5716c26c9f9cefeb769a1073f8fe2d9
Lee Priest <leeclarkepriest@gmail.com>
2023-10-11 07:29:19 -0400

    Working well

--- BatchPDM/BatchPDM.vb
+++ BatchPDM/BatchPDM.vb
@@ -7,8 +7,8 @@ Imports SolidWorks.Interop.swconst
 Public Class BatchPDM
     Implements IEdmAddIn5
 
-    Const LOGPATH As String = "C:\Temp\BatchPDM_Log.txt"
-    Const LOGPATH_ERROR As String = "C:\Temp\BatchPDM_ERRORS_Log.txt"
+    Const RESTARTSWCOUNT As Integer = 250
+    Const LOGPATH As String = "C:\Temp\BatchPDM_Log_"
     Public Const MATERIALDBPATH As String = "C:\PDM\00_Admin\Materiales\MATERIALES_PDM.sldmat"
 
     Public Sub GetAddInInfo(ByRef poInfo As EdmAddInInfo, poVault As IEdmVault5, poCmdMgr As IEdmCmdMgr5) Implements IEdmAddIn5.GetAddInInfo
@@ -23,8 +23,7 @@ Public Class BatchPDM
             poInfo.mlRequiredVersionMajor = 8
             poInfo.mlRequiredVersionMinor = 0
 
-            'Notify the add-in after a file state change event occurs
-            poCmdMgr.AddHook(EdmCmdType.EdmCmd_Menu)
+            poCmdMgr.AddCmd(1, "Batch Set Laser Material", EdmMenuFlags.EdmMenu_MustHaveSelection + EdmMenuFlags.EdmMenu_OnlyFolders)
         Catch
         End Try
 
@@ -32,93 +31,155 @@ Public Class BatchPDM
 
     Public Sub OnCmd(ByRef poCmd As EdmCmd, ByRef ppoData As Array) Implements IEdmAddIn5.OnCmd
 
-        If poCmd.meCmdType = EdmCmdType.EdmCmd_Menu Then 'run the following function when a PostState event occurs (i.e. after the transition has occurred)
+        If poCmd.meCmdType = EdmCmdType.EdmCmd_Menu Then
 
-            Dim confirmList As String = ""
-            For Each folderData As EdmCmdData In ppoData
-                confirmList += folderData.mbsStrData1 + vbNewLine
-            Next
+            If poCmd.mlCmdID = 1 Then
 
-            If MsgBox(confirmList, MsgBoxStyle.OkCancel) = MsgBoxResult.Ok Then
-                FindFiles(poCmd, ppoData)
+                Dim eVault As EdmVault5 = poCmd.mpoVault
+
+                Dim eUser As IEdmUser5 = eVault.CreateUtility(EdmUtility.EdmUtil_UserMgr)
+
+                If eUser.Name.ToLower() = "admin" Then
+
+                    Dim confirmList As String = ""
+                    For Each folderData As EdmCmdData In ppoData
+                        confirmList += folderData.mbsStrData1 + vbNewLine
+                    Next
+
+                    If MsgBox(confirmList, MsgBoxStyle.OkCancel) = MsgBoxResult.Ok Then
+                        FindFiles(poCmd, ppoData, eVault)
+                    End If
+                Else
+
+                    MsgBox("Adming login required to run this function", MsgBoxStyle.Exclamation, "BatchPDM")
+
+                End If
+
             End If
 
         End If
 
     End Sub
 
-    Private Sub FindFiles(ByVal poCmd As EdmCmd, ByRef ppoData As System.Array)
+    Private Sub FindFiles(poCmd As EdmCmd, ByRef ppoData As System.Array, eVault As EdmVault5)
 
-        Dim eVault As EdmVault5 = Nothing
         Dim eFolder As IEdmFolder6 = Nothing
 
         Try
-            eVault = poCmd.mpoVault
-
             Dim processedList As New List(Of String)
 
-            Dim swApp As SldWorks = CreateObject("SldWorks.Application")
+            Dim swApp As SldWorks = StartSW()
 
-            For Each folderData As EdmCmdData In ppoData
-                processedList.Add(folderData.mbsStrData1)
+            Dim count As Integer = 0
+            Dim success As Boolean = True
 
-                eFolder = eVault.GetFolderFromPath(folderData.mbsStrData1)
+            If swApp IsNot Nothing Then
 
-                TraverseFolder(swApp, eFolder)
-            Next
+                For Each folderData In ppoData
 
-            swApp.ExitApp()
+                    If processedList.Contains(folderData.mlObjectID2) = False Then
+
+                        WriteToLog(False, $"Folder ID: {folderData.mlObjectID2}")
+
+                        processedList.Add(folderData.mlObjectID2)
+
+                        eFolder = eVault.GetObject(EdmObjectType.EdmObject_Folder, folderData.mlObjectID2)
+
+                        If eFolder IsNot Nothing Then
+                            TraverseFolder(swApp, count, eFolder)
+                        Else
+                            WriteToLog(True, $"Unable to get folder object with ID: {folderData.mlObjectID2}")
+                        End If
+
+                        eFolder = Nothing
+                    End If
+                Next
+
+                CloseSW(swApp)
+
+
+                If success = True Then
+                    MsgBox($"Successfully processed {count} files")
+
+                    WriteToLog(False, $"Job complete - Successfully processed {count} files")
+                End If
+
+            End If
 
         Catch ex As System.Exception
+            Dim st As New StackTrace(True)
+            st = New StackTrace(ex, True)
 
-            MsgBox("The following error occurred:" & vbNewLine & vbNewLine & ex.Message, MsgBoxStyle.Exclamation, My.Application.Info.AssemblyName)
+            MsgBox($"The following error occurred:{vbNewLine}{vbNewLine}{ex.Message} (Line: {st.GetFrame(0).GetFileLineNumber()})", MsgBoxStyle.Exclamation, My.Application.Info.AssemblyName)
 
         End Try
 
     End Sub
 
-    Sub TraverseFolder(swApp As SldWorks, folder As IEdmFolder5)
+    Sub TraverseFolder(ByRef swApp As SldWorks, ByRef count As Integer, eFolder As IEdmFolder5)
 
-        Dim pdmFilePos As IEdmPos5
-        pdmFilePos = folder.GetFirstFilePosition()
+        If eFolder IsNot Nothing Then
 
-        While pdmFilePos.IsNull = False
-            Dim eFile As IEdmFile5
-            eFile = folder.GetNextFile(pdmFilePos)
+            Dim pdmFilePos As IEdmPos5
+            pdmFilePos = eFolder.GetFirstFilePosition()
 
-            If eFile.Name.Substring(Len(eFile.Name) - 6, 6) = "sldprt" Then
+            While pdmFilePos.IsNull = False
+                Dim eFile As IEdmFile5
+                eFile = eFolder.GetNextFile(pdmFilePos)
 
-                Dim eFileCard As IEdmEnumeratorVariable8 = eFile.GetEnumeratorVariable()
-                Dim variableValue As Object
-                eFileCard.GetVar("Laser", "@", variableValue)
+                If eFile IsNot Nothing Then
 
-                If variableValue.ToString().ToLower() = "x" Then
-                    If eFile.IsLocked = True Then
-                        If eFile.LockedByUser.Name.ToLower() = "admin" Then
-                            SetMaterial(swApp, eFile.LockPath)
+                    If Strings.Right(eFile.Name, 6).ToLower() = "sldprt" Then
 
-                            Dim streamWriter As New StreamWriter(LOGPATH)
-                            streamWriter.WriteLine($"Sucess setting material: {eFile.Name}")
-                            streamWriter.Close()
+                        Dim eFileCard As IEdmEnumeratorVariable8 = eFile.GetEnumeratorVariable()
+                        Dim variableValue As Object = Nothing
+                        If eFileCard IsNot Nothing Then
+                            eFileCard.GetVar("Laser", "@", variableValue)
+                        Else
+                            WriteToLog(True, $"Unable to read 'Laser' property: {eFile.Name}")
                         End If
-                    Else
-                        Dim streamWriter As New StreamWriter(LOGPATH_ERROR)
-                        streamWriter.WriteLine($"Not checked out to admin: {eFile.Name}")
-                        streamWriter.Close()
+
+                        If variableValue IsNot Nothing Then
+                            If variableValue.ToString().ToLower() = "x" Then
+                                If eFile.IsLocked = True Then
+                                    If eFile.LockedByUser.Name.ToLower() = "admin" Then
+                                        SetMaterial(swApp, eFile.LockPath)
+
+                                        count += 1
+
+                                        If count Mod RESTARTSWCOUNT = 0 Then
+
+                                            WriteToLog(False, $"Restart solidworks: {count} files processed")
+                                            CloseSW(swApp)
+                                            swApp = StartSW()
+
+                                            If swApp Is Nothing Then WriteToLog(True, $"Batch did not complete successfully")
+
+                                        End If
+
+                                        WriteToLog(False, $"Success setting material: {eFile.Name}")
+                                    Else
+                                        WriteToLog(True, $"Not checked out to admin: {eFile.Name}")
+                                    End If
+                                End If
+                            End If
+                        End If
                     End If
                 End If
-            End If
 
-        End While
+            End While
 
-        Dim pdmSubFolderPos As IEdmPos5
-        pdmSubFolderPos = folder.GetFirstSubFolderPosition()
+            Dim pdmSubFolderPos As IEdmPos5
+            pdmSubFolderPos = eFolder.GetFirstSubFolderPosition()
 
-        While Not pdmSubFolderPos.IsNull
-            Dim pdmSubFolder As IEdmFolder5
-            pdmSubFolder = folder.GetNextSubFolder(pdmSubFolderPos)
-            TraverseFolder(swApp, pdmSubFolder)
-        End While
+            While Not pdmSubFolderPos.IsNull
+                Dim pdmSubFolder As IEdmFolder5
+                pdmSubFolder = eFolder.GetNextSubFolder(pdmSubFolderPos)
+
+                TraverseFolder(swApp, count, pdmSubFolder)
+            End While
+
+        End If
 
     End Sub
 
@@ -146,5 +207,38 @@ Public Class BatchPDM
 
     End Sub
 
+    Private Function StartSW() As SldWorks
+
+        Dim swApp As SldWorks = Nothing
+        Try
+            swApp = CreateObject("SldWorks.Application")
+        Catch ex As Exception
+            WriteToLog(True, ex.Message)
+        End Try
+
+        If swApp Is Nothing Then WriteToLog(True, "swApp object is null")
+
+        Return swApp
+
+    End Function
+
+    Private Sub CloseSW(swApp As SldWorks)
+        Dim swProcess() As Process = Process.GetProcessesByName("SLDWORKS")
+
+        swApp.ExitApp()
+        swProcess(0).Kill()
+        swProcess(0).WaitForExit()
+    End Sub
+
+    Private Sub WriteToLog(logError As Boolean, message As String)
+
+        Dim messageType As String = " [INFO]"
+        If logError = True Then messageType = "[ERROR]"
+
+        Dim streamWriter As New StreamWriter($"{LOGPATH}{Strings.Format(DateTime.Now, "yyMMdd")}.txt", True)
+        streamWriter.WriteLine($"{messageType} {Strings.Format(DateTime.Now, "hhmmss")}: {message}")
+        streamWriter.Close()
+
+    End Sub
 
 End Class
--- BatchPDM/My Project/Settings.Designer.vb
+++ BatchPDM/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.JobEmailLaunch.My.MySettings
+        Friend ReadOnly Property Settings() As Global.BatchPDM.My.MySettings
             Get
-                Return Global.JobEmailLaunch.My.MySettings.Default
+                Return Global.BatchPDM.My.MySettings.Default
             End Get
         End Property
     End Module
