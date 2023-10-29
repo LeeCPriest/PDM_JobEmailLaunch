0e3e46111ef287167961912d7952789b1ba0bf34
Lee Priest <leeclarkepriest@gmail.com>
2023-10-29 19:55:01 -0400

    Added code to set Codigo property on PostAdd event

--- BatchPDM/My Project/Settings.Designer.vb
+++ BatchPDM/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.BatchPDM.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SerratAutomation.My.MySettings
             Get
-                Return Global.BatchPDM.My.MySettings.Default
+                Return Global.SerratAutomation.My.MySettings.Default
             End Get
         End Property
     End Module
